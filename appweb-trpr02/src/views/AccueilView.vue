d13d1372751019b488454132207cc6c4f3d47817
Maria Germain <mariagermain224@gmail.com>
2024-04-04 17:03:59 -0400

    accueil

--- appweb-trpr02/src/views/AccueilView.vue
+++ appweb-trpr02/src/views/AccueilView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import AccueilForm from '../components/AccueilForm.vue'
+
 </script>
 
 <template>
-    <h1>Accueil</h1>
+    <h1>Votre objectif: survivre à 5 missions en obtenat le plus de crédits galactiques</h1>
+    <AccueilForm/>
 </template>
 
 <style scoped>
