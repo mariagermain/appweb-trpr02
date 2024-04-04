8dcc822a578b73648099aa0616d35bf608d405d9
Maria Germain <mariagermain224@gmail.com>
2024-04-04 16:46:45 -0400

    supressions fichiers de base

--- appweb-trpr02/src/router/index.ts
+++ appweb-trpr02/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import AccueilView from '@/views/AccueilView.vue'
 
 const router = createRouter({
