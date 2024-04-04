e032c9431ed295fa76d73ec190d04d04ddbfe12b
TheoHautois <theo.hautois@gmail.com>
2024-04-04 16:03:29 -0400

    Ajout des routes /accueil et /score

--- appweb-trpr02/src/router/index.ts
+++ appweb-trpr02/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AccueilView from '@/views/AccueilView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +8,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: AccueilView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/score',
+      name: 'score',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/ScoreView.vue')
     }
   ]
 })
