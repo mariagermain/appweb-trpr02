9ca4e65a645ac2455faf33fb94862a64a9cbd370
Maria Germain <mariagermain224@gmail.com>
2024-04-07 22:09:31 -0400

    GameView débuté

--- appweb-trpr02/src/views/GameView.vue
+++ appweb-trpr02/src/views/GameView.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-
-let showLoadingError = ref(false);
-
-function loadingError(){
-    showLoadingError.value = true;
-}
+import Actions from '../components/Actions.vue'
+import Missions from '../components/Missions.vue'
+import PlayerInfos from '../components/PlayerInfos.vue'
 
 </script>
 
 <template>
-
+    <div class="container">
+        <div class="row">
+            <Actions/>
+            <Missions/>
+        </div>
+        <div class="row">
+            <PlayerInfos/>
+            <PlayerInfos/>
+        </div>
+    </div>
 </template>
 
 <style scoped>
