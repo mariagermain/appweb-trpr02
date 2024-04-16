19e2f04795a2893702d31f8f4e7b27b9fb89873a
Maria Germain <mariagermain224@gmail.com>
2024-04-16 13:59:30 -0400

    Create AccueilView.vue

---
+++ appweb-trpr02/src/views/AccueilView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import AccueilForm from '../components/AccueilForm.vue'
+import ErrorMsg from '../components/ErrorMsg.vue'
+import { useRouter, type Router } from 'vue-router';
+import type { Ship, Player } from '@/scripts/Types';
+
+let ROUTER : Router = useRouter();
+
+let showLoadingError = ref(false);
+
+let playerName: Ref<string> = ref("");
+let ship: Ref<Ship>;
+
+function loadingError(){
+    showLoadingError.value = true;
+}
+
+function submitForm(playerName : String, ship : Ship){
+    ROUTER.push({ name : 'game', params : { playerName : playerName, ship : ship.name}});
+}
+
+</script>
+
+<template>
+    <h3>Votre objectif: survivre à 5 missions en obtenant le plus de crédits galactiques</h3>
+    <ErrorMsg :show="showLoadingError" message="Impossible de contacter l'API."></ErrorMsg>
+    <suspense>
+        <AccueilForm @loading-error="loadingError()" @submit-form="submitForm(playerName.value, ship)"/>
+    </suspense>
+</template>
+
+<style scoped>
+</style>
