880c335084bd681591c9503af7a5d7dd5439d1a7
TheoHautois <theo.hautois@gmail.com>
2024-04-18 15:29:37 -0400

    Ajout de l'affichage des erreurs dans le formulaire de l'accueil

--- src/components/AccueilForm.vue
+++ src/components/AccueilForm.vue
@@ -3,7 +3,7 @@ import type { Ship } from "../scripts/Types";
 import AppService from "../../AppService"
 import { useRouter, type Router } from "vue-router";
 import { ref, type Ref } from "vue";
-
+import ErrorList from '../components/ErrorList.vue'
 const emit = defineEmits(['loading-error', 'submit-form']);
 
 
@@ -15,24 +15,34 @@ let ships : Ship[] = await APP_SERVICE.getShips().catch(() => {
     emit('loading-error');
 }).then(it => it || []);
 
-let playerName :string;
+// form
+let playerName :string = "";
 let selectedShip :Ship;
 
+// erreurs
+let errorList:Ref<string[]> = ref([]);
+
 function validateForm():boolean{
-    if (playerName.trim().length <= 0) return false;
-    if (selectedShip == undefined || selectedShip == null) return false;
-    return true;
+    errorList.value = [];
+    if (playerName.trim().length <= 0)
+        errorList.value.push("Le nom du joueur est obligatoire.") ;
+    
+    if (selectedShip == undefined || selectedShip == null) 
+        errorList.value.push("Veuillez choisir un vaisseau.");
+    
+    return errorList.value.length == 0;
 }
 
 function submitForm():void{
-    if (validateForm()) emit('submit-form',playerName, selectedShip );
+    if (validateForm()) 
+        emit('submit-form',playerName, selectedShip );
 }
-
 
 </script>
 
 <template>
     <form class="form-outline w-50 mx-auto border border-secondary rounded p-3">
+        <ErrorList title="Impossible de débuter la partie." :errors="errorList"></ErrorList>
         <div class="form-group pb-3">
             <input v-model="playerName" type="text" class="form-control" placeholder="Votre nom" aria-label="Username" aria-describedby="basic-addon1">
         </div>
