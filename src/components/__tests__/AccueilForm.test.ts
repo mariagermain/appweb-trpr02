e253cd4d66959fe4509dec9937083fa123f869e8
Maria Germain <mariagermain224@gmail.com>
2024-04-25 14:10:55 -0400

    gr
    gr

--- src/components/__tests__/AccueilForm.test.ts
+++ src/components/__tests__/AccueilForm.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { describe, it, expect, beforeAll, afterAll, vi } from 'vitest'
 import { flushPromises, mount } from '@vue/test-utils'
 import AccueilForm from '../AccueilForm.vue'
 
@@ -7,6 +7,8 @@ import { getShips, networkError } from '../../../tests/mocks/AppServiceMock'
 import { afterEach } from 'node:test'
 import { defineComponent } from 'vue'
 import {ships} from '../../../tests/data/ships'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 
 // Pour encadrer notre composent dans un <Suspense>
 const testComponent = defineComponent({
@@ -56,3 +58,51 @@ describe('AccueilForm', () => {
     })
 })
 
+<<<<<<< Updated upstream
+=======
+})
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes : routes
+})
+describe('Routes', () => {
+
+    it('Doit pouvoir naviguer sur la page des publications.', async () => {
+      router.push('/') // S'assurer de commencer avec une route connue
+      await router.isReady() // Attendre que le routeur soit prêt
+  
+      const wrapper = mount(AccueilForm, {
+          global: {
+            plugins: [router]
+          }
+      })
+  
+      const routerSpy = vi.spyOn(router, 'push')
+  
+      // Trouver le lien par son texte et simuler un clic
+      const linkPostsEl = wrapper.find('#publications') // Adapter le sélecteur selon ton besoin
+      await linkPostsEl.trigger('click')
+  
+      expect(routerSpy).toHaveBeenCalledWith('/')
+    })
+  
+    it('Doit pouvoir naviguer sur la page à propos.', async () => {
+      router.push('/') // Reset la route au début de chaque test
+      await router.isReady()
+  
+      const wrapper = mount(AccueilForm, {
+          global: {
+            plugins: [router]
+          }
+      })
+  
+      const routerSpy = vi.spyOn(router, 'push')
+  
+      const linkAboutEl = wrapper.find('#apropos') // Adapter le sélecteur selon ton besoin
+      await linkAboutEl.trigger('click')
+  
+      expect(routerSpy).toHaveBeenCalledWith({ name: 'About' })
+    })
+  })
+>>>>>>> Stashed changes
